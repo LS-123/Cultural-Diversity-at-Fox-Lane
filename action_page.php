2c611e422fb2a9089e6ddcfb4b8477913a796c73
LS-123 <lydiasavitt@gmail.com>
2020-03-22 12:06:19 -0400

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,18 @@
+<!--Next time: Learn how to use PHP file
+			- Connect to domain and host site
+			- Add more countries
+-->
+
+
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link href="Design.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+</body>
+</html>
